ec5256f140e4d3d196c7caf9d19c61344a84b129
juanCano <jcanor@unal.edu.co>
2022-02-14 12:25:28 -0500

    Primer componente creado manualmente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Mascotas } from './mascotas/mascotas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Mascotas
   ],
   imports: [
     BrowserModule
